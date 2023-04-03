2d4a2eef5c88e53d7707222b477783c394a97f08
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-04-03 12:43:45 -0300

    Se agrega la logica para mostrar el horario en formato AM y PM del 0 al 12

--- js/app.js
+++ js/app.js
@@ -7,11 +7,24 @@ const mes = fechaActual.getMonth(); //0-11
 const año = fechaActual.getFullYear(); //2023
 let pFecha = document.getElementById('fecha');
 let pHora = document.getElementById('hora');
+let tipoHorario = 'AM'
 const dias = ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado']
 const meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
 
 pFecha.innerHTML = `${dias[diaSemana]} ${dia} de ${meses[mes]} del ${año}`
-pHora.innerHTML = `${fechaActual.getHours()}:${fechaActual.getMinutes()}:${fechaActual.getSeconds()}`
+pHora.innerHTML = `${hora()}:${fechaActual.getMinutes()}:${fechaActual.getSeconds()} ${tipoHorario}`
+
+function hora(){
+    let horaActual = fechaActual.getHours()
+    let horaPm;
+    if(horaActual > 12){
+        horaPm = '0' + (horaActual - 12);
+        tipoHorario = 'PM';   
+        return horaPm;
+    }else{
+        return horaActual;
+    }
+}
 
 }
 
