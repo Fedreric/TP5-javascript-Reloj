87d6507302d335c6987191d9ba81e2733dedd783
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-04-03 13:07:34 -0300

    se agrega un 0 adelante en los minutos y segundos cuando son menores a 10

--- js/app.js
+++ js/app.js
@@ -1,31 +1,54 @@
-function obtenerHora(){
-//obtener la fecha actual
-let fechaActual = new Date();
-const dia = fechaActual.getDate(); //1-31
-const diaSemana = fechaActual.getDay(); //0-6
-const mes = fechaActual.getMonth(); //0-11
-const año = fechaActual.getFullYear(); //2023
-let pFecha = document.getElementById('fecha');
-let pHora = document.getElementById('hora');
-let tipoHorario = 'AM'
-const dias = ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado']
-const meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
+function obtenerHora() {
+    //obtener la fecha actual
+    let fechaActual = new Date();
+    const dia = fechaActual.getDate(); //1-31
+    const diaSemana = fechaActual.getDay(); //0-6
+    const mes = fechaActual.getMonth(); //0-11
+    const año = fechaActual.getFullYear(); //2023
+    let pFecha = document.getElementById('fecha');
+    let pHora = document.getElementById('hora');
+    let tipoHorario = 'AM'
+    const dias = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado']
+    const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 
-pFecha.innerHTML = `${dias[diaSemana]} ${dia} de ${meses[mes]} del ${año}`
-pHora.innerHTML = `${hora()}:${fechaActual.getMinutes()}:${fechaActual.getSeconds()} ${tipoHorario}`
+    pFecha.innerHTML = `${dias[diaSemana]} ${dia} de ${meses[mes]} del ${año}`
+    pHora.innerHTML = `${hora()}:${minutos()}:${segundos()} ${tipoHorario}`
 
-function hora(){
-    let horaActual = fechaActual.getHours()
-    let horaPm;
-    if(horaActual > 12){
-        horaPm = '0' + (horaActual - 12);
-        tipoHorario = 'PM';   
-        return horaPm;
-    }else{
-        return horaActual;
+    function hora() {
+        let horaActual = fechaActual.getHours()
+        let horaPm;
+        if (horaActual === 12) {
+            tipoHorario = 'PM';
+            return horaActual;
+        } else if (horaActual > 12) {
+            horaPm = '0' + (horaActual - 12);
+            tipoHorario = 'PM';
+            return horaPm;
+        } else {
+            return horaActual;
+        }
     }
-}
+    function minutos() {
+        let minutosActual = fechaActual.getMinutes();
+        let primerosMinutos;
+        if (minutosActual < 10) {
+            primerosMinutos = '0' + (minutosActual);
+            return primerosMinutos;
+        } else {
+            return minutosActual;
+        }
+    }
+    function segundos() {
+        let segundosActual = fechaActual.getSeconds();
+        let primerosSegundos;
+        if (segundosActual < 10) {
+            primerosSegundos = '0' + (segundosActual);
+            return primerosSegundos;
+        } else {
+            return segundosActual;
+        }
+    }
 
 }
 
-setInterval(obtenerHora,1000)
+setInterval(obtenerHora, 1000)
